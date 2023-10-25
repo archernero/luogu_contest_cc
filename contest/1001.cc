dab3c458cc9b17c676dd9b61c5c8adb59acaa6f0
xiaotongl <2806838524@qq.com>
2023-10-26 02:56:17 +0800

    23456

---
+++ contest/1001.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void solution(long long a, long long b) {
+    long long ans = a + b;
+    cout << ans << "\n";
+}
+
+int main(int argc, char const *argv[])
+{
+    // 构造输入
+    long long a;
+    long long b;
+    cin >> a;
+    cin >> b;
+    // 解答
+    solution(a, b);
+    return 0;
+}
