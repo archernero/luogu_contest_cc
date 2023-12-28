c8d2dbd37af8d563572235027a83268e6ba572dd
neroxtliu <neroxtliu@tencent.com>
2023-12-28 11:16:30 +0800

    23456

--- test/union_find_test.cc
+++ test/union_find_test.cc
@@ -27,27 +27,46 @@ void test_vector() {
   cout << a.back() << endl;
 }
 
-int findCircleNum(vector<vector<int> >& is_connected) {
-    // leetcode 547
-    int nums = is_connected.size();
-    UnionFind uf(nums);
-    for (int i = 0; i < nums; i++) {
-        for (int j = 0; j < i; j++) {
-            if (is_connected[i][j]) {
-                uf.union_func(i, j);
-            }
-        }
+int findCircleNum(vector<vector<int>>& is_connected) {
+  // leetcode 547
+  int nums = is_connected.size();
+  UnionFind uf(nums);
+  for (int i = 0; i < nums; i++) {
+    for (int j = 0; j < i; j++) {
+      if (is_connected[i][j]) {
+        uf.union_func(i, j);
+      }
     }
-    int ans = 0;
-    for (int i = 0; i < nums; i++) {
-        if (i == uf.find(i)) {
-            ans += 1;
-        }
+  }
+  int ans = 0;
+  for (int i = 0; i < nums; i++) {
+    if (i == uf.find(i)) {
+      ans += 1;
     }
-    return ans;
+  }
+  return ans;
 }
 
-int main(int argc, char const *argv[]) {
+bool validTree(int n, vector<vector<int>>& edges) {
+  // leectcode 261
+  UnionFind uf(n);
+  for (auto tmp: edges) {
+    int f = tmp[0];
+    int t = tmp[1];
+    if (uf.find(f) != uf.find(t)) {
+      uf.union_func(f, t);
+    } else {
+      return false;
+    }
+  }
+  int count = 0;
+  for (int i = 0; i < n; i++) {
+    if (i == uf.find(i)) count += 1;
+  }
+  return count == 1;
+}
+
+int main(int argc, char const* argv[]) {
   vector<string> a = {"1", "2", "3", "4", "5"};
 
   // vector<string>::const_iterator iter = find_if(a.cbegin(), a.cend(), [](const string& i){
